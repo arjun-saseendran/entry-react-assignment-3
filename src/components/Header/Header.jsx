854a51b5551b7c4ac20a63f4dbcf364be0d1da7a
arjun-saseendran <arjunsaseendran@icloud.com>
2024-10-12 15:01:45 +0530

    add ProductCard.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,25 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" style={{ background: "#041C32" }}>
       <Container>
-        <Navbar.Brand href="#home">ShopNow</Navbar.Brand>
+        <Navbar.Brand className="text-white" href="/">
+          ShopNow
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Cart</Nav.Link>
+            <Link className="nav-link text-white" href="#home">
+              Home
+            </Link>
+            <Link className="nav-link text-white" href="#link">
+              Cart
+            </Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import axios from "axios";
+import { Container, Row, Col } from "react-bootstrap";
+
+function ProductCard() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => setProducts(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <Container className="m-5">
+      <Row>
+        {products.map((product) => (
+          <Col xs={12} sm={6} md={4} xl={3} key={product.id}>
+            <Card style={{ background: "#8EACCD", marginBottom: '24px' }} >
+              <Card.Img
+                variant="top"
+                src={product.image}
+                style={{
+                  height: "200px",
+                  objectFit: "contain",
+                  padding: "10px",
+                }}
+              />
+              <Card.Body>
+                <Card.Title
+                  style={{
+                    maxHeight: "20px",
+                    overflow: "hidden",
+                    color: "#2D4263",
+                  }}
+                >
+                  {product.title}
+                </Card.Title>
+                <Card.Text
+                  style={{
+                    maxHeight: "78px",
+                    overflow: "hidden",
+                    color: "#2D4263",
+                  }}
+                >
+                  {product.description}
+                </Card.Text>
+                <Card.Text
+                  style={{
+                    maxHeight: "78px",
+                    overflow: "hidden",
+                    color: "#2D4263",
+                  }}
+                >
+                  ${product.price}
+                </Card.Text>
+                <Button
+                  variant="dark"
+                  className="m-1"
+                  style={{ background: "#2D4263" }}
+                >
+                  Add to cart
+                </Button>
+                <Button
+                  variant="dark"
+                  className="m-1"
+                  style={{ background: "#2D4263" }}
+                >
+                  View product
+                </Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+}
+
+export default ProductCard;
